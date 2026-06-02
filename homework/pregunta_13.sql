2b6605719c12e4930eafb366ae04f07f1fb1b072
Maribel Cortes Camacho <marcortesca@unal.edu.co>
2026-06-01 20:29:13 -0500

    solucion lab09

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -43,3 +43,10 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
+SELECT
+  K0,
+  (ROUND(AVG(c12) * 100, 0) / 100.0) AS "avg(c12)"
+FROM tbl1
+WHERE c13 > 400
+GROUP BY K0
+ORDER BY K0
